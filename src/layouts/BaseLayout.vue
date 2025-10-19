4c2abe330fe42da6bfcb2736d5a372241bc18f8a
cristianvasquezc <cristianvasquez1312@gmail.com>
2025-10-19 18:00:56 -0500

    fix: agregar clases de estilo al contenedor principal en BaseLayout.vue

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -7,7 +7,7 @@ import Navbar from '@/components/layout/Navbar.vue';
 <template>
     <div>
         <Navbar />
-        <main>
+        <main class="w-full px-2">
             <slot />
         </main>
         <Footer />
