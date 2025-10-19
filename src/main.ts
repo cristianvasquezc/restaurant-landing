694c24fbed1d6a3349c0391c21b11e946b190437
cristianvasquezc <cristianvasquez1312@gmail.com>
2025-10-19 15:46:16 -0500

    feat: agregar uso de naive-ui en la aplicación

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import naive from 'naive-ui';
 import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './router';
 import './style.css';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(naive).mount('#app');
