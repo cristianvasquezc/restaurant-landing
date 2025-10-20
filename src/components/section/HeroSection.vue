dbb1edb51c1ff472584d617282ea1151fd8233aa
cristianvasquezc <cristianvasquez1312@gmail.com>
2025-10-20 16:32:58 -0500

    feat: agregar sección Hero con imagen de fondo y contenido de bienvenida

---
+++ src/components/section/HeroSection.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="relative text-white h-[calc(100vh-71px)]">
+        <img src="@/assets/images/background.webp" alt="Background"
+            class="absolute inset-0 w-full h-full object-cover object-center" />
+
+        <div class="absolute inset-0 bg-black/20"></div>
+
+        <div class="relative z-10 flex items-center justify-center h-full px-5">
+            <div class="w-full max-w-6xl mx-auto">
+                <div class="max-w-lg space-y-5">
+                    <h1 class="text-3xl md:text-5xl leading-tight font-black">
+                        Welcome!
+                        <br />
+                        We Made Delicious Food for You
+                    </h1>
+                    <p class="text-lg">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    </p>
+                    <n-button round color="white" text-color="black">
+                        Order Online
+                    </n-button>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
