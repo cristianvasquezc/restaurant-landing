12417df37601602660440d2b7514b52dc6730484
cristianvasquezc <cristianvasquez1312@gmail.com>
2025-10-20 15:20:47 -0500

    feat: agregar componente Subtitle con soporte para texto y alineación personalizable

---
+++ src/components/ui/Subtitle.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { twMerge } from 'tailwind-merge';
+import { computed } from 'vue';
+
+const props = defineProps<{
+    text?: string
+    align?: 'left' | 'center' | 'right'
+    class?: string
+}>()
+
+const subtitleClass = computed(() =>
+    twMerge(
+        'font-bold text-3xl md:text-5xl text-white mb-5',
+        props.align ? `text-${props.align}` : 'text-center',
+        props.class
+    )
+)
+</script>
+
+<template>
+    <h2 :class="subtitleClass">
+        <slot>{{ text }}</slot>
+    </h2>
+</template>
