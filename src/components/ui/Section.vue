614b220d8f9738536f2cf081c05245d854ed1c2b
cristianvasquezc <cristianvasquez1312@gmail.com>
2025-10-20 14:47:20 -0500

    feat: agregar componente Section con soporte para clases personalizadas y estilos

---
+++ src/components/ui/Section.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { twMerge } from 'tailwind-merge';
+import { computed } from 'vue';
+
+const props = defineProps<{
+    id?: string
+    class?: string
+}>()
+
+const sectionClass = computed(() =>
+    twMerge('bg-primary py-20 px-5', props.class)
+)
+</script>
+
+<template>
+    <section :id="props.id" :class="sectionClass">
+        <div class="max-w-6xl mx-auto">
+            <slot />
+        </div>
+    </section>
+</template>
