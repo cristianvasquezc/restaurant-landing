9d0ba6620ff10ada14916ee76dde10c2312ace37
cristianvasquezc <cristianvasquez1312@gmail.com>
2025-10-20 16:34:11 -0500

    feat: reestructurar HomeView para incluir secciones Hero y Welcome

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+import HeroSection from '@/components/section/HeroSection.vue';
+import WelcomeSection from '@/components/section/WelcomeSection.vue';
+import BaseLayout from '@/layouts/BaseLayout.vue';
 
 
 </script>
 
 <template>
-    <h1>HomeView</h1>
+    <BaseLayout>
+        <HeroSection />
+
+        <WelcomeSection />
+    </BaseLayout>
 </template>
