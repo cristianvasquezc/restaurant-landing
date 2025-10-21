211987e4c02afb7e0e4a7432f1ac506f5d97a3d1
cristianvasquezc <cristianvasquez1312@gmail.com>
2025-10-21 17:08:34 -0500

    feat: agregar sección support con campo de entrada y boton suscribir

---
+++ src/components/section/SupportSection.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Section from '@/components/ui/Section.vue';
+import Subtitle from '@/components/ui/Subtitle.vue';
+
+
+</script>
+
+<template>
+    <Section id="welcome" class="bg-white py-10">
+        <Subtitle class="text-black">
+            For more Support
+        </Subtitle>
+
+        <n-flex align="center" justify="center">
+            <n-input round placeholder="Enter Email" class="border"
+                style="--n-text-color: black; --n-placeholder-color: black; max-width: 300px;" />
+            <n-button round color="black" text-color="white">
+                Subscribe
+            </n-button>
+        </n-flex>
+    </Section>
+</template>
