13f605970d6abf211a502756980f40b68950ef00
cristianvasquezc <cristianvasquez1312@gmail.com>
2025-10-23 11:43:16 -0500

    Agregando pagina 404

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import BaseLayout from '@/layouts/BaseLayout.vue';
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+    <BaseLayout>
+        <section class="h-[calc(100vh-80px)] flex flex-col items-center justify-center bg-primary text-white">
+            <h1 class="text-7xl font-bold">404</h1>
+            <n-p>Page not found</n-p>
+
+            <RouterLink to="/">
+                <n-button round color="white" text-color="black">
+                    Go back home
+                </n-button>
+            </RouterLink>
+        </section>
+    </BaseLayout>
+</template>
