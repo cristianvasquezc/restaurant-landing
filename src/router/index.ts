f621b892d7c83e619dbaa400d2b2f8edad2e87db
cristianvasquezc <cristianvasquez1312@gmail.com>
2025-10-23 11:45:12 -0500

    Agregando ruta not-found

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import HomeView from '@/views/HomeView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
 
-const routes = [{ path: '/', component: HomeView }];
+const routes = [
+  { path: '/', component: HomeView, name: 'home' },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: NotFoundView,
+  },
+];
 
 export const router = createRouter({
   history: createWebHistory(),
