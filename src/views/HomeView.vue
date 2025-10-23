e0b9dbfc21183c91fcf0a0745a049266b651cd7c
cristianvasquezc <cristianvasquez1312@gmail.com>
2025-10-23 11:33:46 -0500

    Importando secciones

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import BaseLayout from '@/layouts/BaseLayout.vue';
 
+import ClientsSection from '@/components/section/ClientsSection.vue';
 import FranchiseSection from '@/components/section/FranchiseSection.vue';
 import HeroSection from '@/components/section/HeroSection.vue';
+import MenuSection from '@/components/section/MenuSection.vue';
 import QualityFoodSection from '@/components/section/QualityFoodSection.vue';
 import SupportSection from '@/components/section/SupportSection.vue';
 import WelcomeSection from '@/components/section/WelcomeSection.vue';
 import WhereFindUsSection from '@/components/section/WhereFindUsSection.vue';
-
 
 
 </script>
@@ -18,9 +19,13 @@ import WhereFindUsSection from '@/components/section/WhereFindUsSection.vue';
 
         <WelcomeSection />
 
+        <MenuSection />
+
         <QualityFoodSection />
 
         <FranchiseSection />
+
+        <ClientsSection />
 
         <SupportSection />
 
